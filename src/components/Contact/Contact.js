0fc1c97b9ae4e183222217a1c41d1bf779cb9e1b
yutomaeda5510 <yutomaeda5510@gmail.com>
2020-04-14 23:53:38 +0900

    add

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,1 +1,8 @@
+import React from "react";
+import { Typography, Layout, Avatar } from "antd";
+import styled from "styled-components";
+
+const { Title } = Typography;
+const { Content } = Layout;
+
 
